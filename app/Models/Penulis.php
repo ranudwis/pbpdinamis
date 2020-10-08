b75957853551e90592a83c6c4a9c8ac6f90c1761
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-10-08 08:07:34 +0700

    Buat model

---
+++ app/Models/Penulis.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Penulis extends Model
+{
+    use HasFactory;
+
+    protected $table = 'penulis';
+
+    protected $primaryKey = 'idpenulis';
+}
