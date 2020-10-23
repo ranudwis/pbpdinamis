d28eb5ec410c94b28d4b843c4281b2709c510892
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-10-23 21:51:22 +0700

    Guarding rute

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,6 +53,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'penulis' => \App\Http\Middleware\PenulisMiddleware::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
