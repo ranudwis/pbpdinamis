609feab202b5968d92e667e0dc7b788a4b53104c
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-12-16 18:50:15 +0700

    Upload postkategori

---
+++ mobile/src/views/PostKategori.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+        Ini halaman postingan dengan kategori tertentu {{ $route.params }}
+    </v-container>
+</template>
+
+<script>
+import api from '@/api'
+
+export default {
+    data: () => ({
+        post: null
+    }),
+
+    async created() {
+        let post = await api.get('post/kategori/' + this.$route.params.id)
+
+        this.post = post.data
+    }
+}
+</script>
