3f9c00a7ad55b1f1cf822e5606f51bab74c0e3a4
Dandi Arimansyah <dandimansyah@gmail.com>
2020-12-22 16:15:49 +0700

    Edit dikit di komentar tapi masih belum selesai

--- mobile/src/views/DetailPost.vue
+++ mobile/src/views/DetailPost.vue
@@ -23,7 +23,7 @@
         <v-card
         max-width="450"
         class="mx-2 my-4"
-        color="lime lighten-5"
+        color="amber lighten-5"
         >
         <h2 class="text-center my-2">Komentar</h2>
 
@@ -32,8 +32,8 @@
                 v-for="p in post.komentar"
                 :key="p.idkomentar"
             >
-                <h3>{{p.penulis.user.nama}}</h3>
-                <p>{{ p.isi}}</p>
+                <h3 class="mx-2">{{ p.penulis.user.nama }}</h3>
+                <p  class="mx-2">{{ p.isi }}</p>
             </v-card>
         </v-container>
         </v-card>
@@ -48,38 +48,6 @@ export default {
     data: () => ({
         post: null,
         komentar: null,
-
-        items: [
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          title: 'Brunch this weekend?',
-          subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-          subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-          title: 'Birthday gift',
-          subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-          title: 'Recipe to try',
-          subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
-        },
-      ],
     }),
 
     methods: {
